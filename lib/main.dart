68f5fb9a6b6c734779336a2e4579b7efbe903154
Tanner <tdyer@hawk.iit.edu>
2020-07-29 20:40:38 -0500

    created question class to eliminate separate question/answer lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -28,18 +29,13 @@ class _QuizPageState extends State<QuizPage> {
 
   int questionNum = 0;
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
-  List<bool> answers = [false, true, true];
 
-  bool checkAnswer(bool answer) {
-    if (answer == answers[questionNum % answers.length])
-      return true;
-    return false;
-  }
+  List<Question> questionList = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
+  ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +49,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNum % questions.length],
+                questionList[questionNum % questionList.length].question,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -84,7 +80,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //true was chosen
                 setState(() {
-                  if (checkAnswer(true))
+                  if (questionList[questionNum % questionList.length].checkAnswer(true))
                     print('correct');
                   else
                     print('incorrect');
@@ -115,7 +111,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //false was chosen
                 setState(() {
-                  if (checkAnswer(false))
+                  if (questionList[questionNum % questionList.length].checkAnswer(false))
                     print('correct');
                   else
                     print('incorrect');
---
+++ lib/question.dart
@@ -0,0 +1,18 @@
+class Question {
+
+  String question;
+  bool answer;
+
+  // constructor
+  Question({String q, bool a}) { 
+    question = q;
+    answer = a;
+  }
+
+  bool checkAnswer(bool a) {
+    if (a == answer)
+      return true;
+    return false;
+  }
+
+}
